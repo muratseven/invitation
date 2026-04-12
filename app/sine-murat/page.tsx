3b33b2791faaa2620cb002b753c7bbff1c6d3d4d
murat seven <muratseven50@gmail.com>
2026-04-12 15:23:08 +0300

    bug-fixed

--- app/sine-murat/page.tsx
+++ app/sine-murat/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 
-// ─── Countdown ───────────────────────────────────────────────────────────────
+// ─── Countdoswn ───────────────────────────────────────────────────────────────
 
 type Countdown = {
   days: number;
